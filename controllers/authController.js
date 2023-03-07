ff4f571c09a19475c5c7b22708c220c0189a4dd1
emerlock <5019530+emerlock@users.noreply.github.com>
2023-03-07 11:33:35 -0500

    add old logic back for jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,28 +1,24 @@
 const User = require('../models/userModel')
-const { hashPassword, comparePassword, createJwtPromise} = require('../util/utils')
+const { hashPassword, comparePassword, createJwt} = require('../util/utils')
 
 const loginUser = async (req, res) => {
     const { username, password } = req.body
     if (!username || !password) return res.status(400).json({ msg: "username, or password not supplied" })
 
-    try {
-        // look for unique user in db with username
-        const user = await User.findOne({ username })
+    // look for unique user in db with username
+    const user = await User.findOne({ username }).catch(err => console.log(err.message))
 
-        if (!user) return res.status(401).json({msg: "User not found"})
-        
-        // look compare supplied password to user's hashed password
-        const isValid = await comparePassword(password, user.password)
+    if (!user) return res.status(401).json({msg: "User not found"})
 
-        // if valid issue a jwt for the user
-        if (isValid) {
-            const jwt = await createJwtPromise(user)
-            res.status(200).json({ success: true, currentUser: { userId: user._id, username: user.username }, tokens: jwt.token, expiresIn: jwt.expires, msg: "User has been logged in" })
-        } else {
-            res.status(400).json({msg: "Credentials not valid"})
-        }
-    } catch (err) {
-        console.log(err.message)
+    // look compare supplied password to user's hashed password
+    const isValid = await comparePassword(password, user.password)
+
+    // if valid issue a jwt for the user
+    if (isValid) {
+        const jwt = createJwt(user)
+        res.status(200).json({ success: true, currentUser: { userId: user._id, username: user.username }, tokens: jwt.token, expiresIn: jwt.expires, msg: "User has been logged in" })
+    } else {
+        res.status(400).json({msg: "Credentials not valid"})
     }
 }
 
@@ -30,22 +26,22 @@ const registerUser = async (req, res) => {
     const { username, password } = req.body
     if (!username || !password ) return res.status(400).json({ msg: "Username, or password not supplied" })
 
-    try {
-        const userExists = await User.findOne({ username })
+    const userExists = await User.findOne({ username }).catch(err => console.log(err.message))
 
-        if (userExists) return res.status(400).json({ msg: "Username has been taken "})
+    if (userExists) return res.status(400).json({ msg: "Username has been taken "})
 
-        const hashedPassword = await hashPassword(password)
+    const hashedPassword = await hashPassword(password)
 
-        const user = await User.create({
-            username, 
-            password: hashedPassword,
-        })
-        const jwt = await createJwtPromise(user)
-        res.status(201).json({ success: true, tokens: jwt.token, expiresIn: jwt.expires, msg: "User has been created!" })
-    } catch (err) {
-        console.log(err.message)
-    }
+    const user = await User.create({
+        username, 
+        password: hashedPassword,
+    }).catch(err => console.log(err.message))
+
+    // reminder for me to look into async creation
+    const jwt = createJwt(user)
+
+    res.status(201).json({ success: true, tokens: jwt.token, expiresIn: jwt.expires, msg: "User has been created!" })
+        
 }
 
 module.exports = {
--- util/utils.js
+++ util/utils.js
@@ -39,8 +39,33 @@ const createJwtPromise = (user) => {
     }) 
 }
 
+// pass in the user object from the MongoDB DB to create JWT
+const createJwt = (user) => {
+    const _id = user._id
+
+    // JWT will expire in Three days for example
+    const maxAge = '3d'
+
+    // Payload will contain the user name, user id in sub, and issued at date
+    const payload = {
+        user: user.username,
+        sub: _id,
+        iat: Date.now(),
+    }
+
+    // sign the new JWT given (payload, secret string, and options)
+    const token = jwt.sign(payload, process.env.SECRET_STRING, { expiresIn: maxAge } )
+
+    // return Bearer + token (what the frontend is looking for to store in localStorage)
+    return {
+        token: "Bearer " + token,
+        expires: maxAge,
+    }
+}
+
 module.exports = {
     hashPassword,
     comparePassword,
     createJwtPromise,
+    createJwt,
 }
